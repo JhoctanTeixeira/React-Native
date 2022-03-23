674ab9554b8f4ac3e2739fd76cb85f9643942735
Jhoctan Teixeira <rm88206@fiap.com.br>
2022-03-23 10:58:45 -0300

    .

---
+++ Aula5/index.js
@@ -0,0 +1,50 @@
+// var x = 1
+// console.log(x)
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// x = "Ale"
+// console.log(x)
+// let x = 1
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// if (x === 1) {
+//     x = 2
+//     console.log(x);
+// }
+
+// console.log(x);
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// const x = 10 
+// console.log(x)
+// x = 20 
+// console.log(x)
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// let a = 10 
+// let b = 20 
+
+// let msg1 = "A = " + a + " B = " + b
+// console.log(msg1)
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// let msg2 = `A = ${a} B = ${b}`
+// console.log(msg2)
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// function soma(a, b) {
+//     return a + b
+// }
+// console.log(soma (3 , 4))
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// function soma(a, b = 0) {
+//     return a + b
+// }
+// console.log(soma (3))
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// let digaOla = function(nome) {
+//     console.log(`Ola, ${nome}`)
+// }
+// digaOla("Ale")
+// console.log(digaOla)

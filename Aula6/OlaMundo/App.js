46a722366403931aad78d4b29d09e4fdc0fb82cd
Jhoctan Teixeira <rm88206@fiap.com.br>
2022-03-29 09:30:57 -0300

    Testes Hello Word

--- Aula6/OlaMundo/App.js
+++ Aula6/OlaMundo/App.js
@@ -1,19 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello Word !</Text>
+
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },

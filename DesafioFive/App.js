764cafffd4ae42a6a9120b3d1f4250fa2e2c5f6b
Jhoctan Teixeira <rm88206@fiap.com.br>
2022-04-06 12:05:04 -0300

    Complete

--- DesafioFive/App.js
+++ DesafioFive/App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {
+  Alert,
+  Button,
+  StatusBar,
+  StyleSheet,
+  TextInput,
+  View
+} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      txtPeso: "",
+      txtAltura: ""
+    }
+  }
+
+  botaoCalcular = _ => {
+    const { txtPeso, txtAltura } = this.state
+    const peso = parseFloat(txtPeso)
+    const altura = parseFloat(txtAltura)
+    if (Number.isNaN(peso) || Number.isNaN(altura)) {
+      Alert.alert("Erro", "Preencha os campos corretamente.")
+      return
+    }
+    const imc = peso / altura ** 2
+    let categoria = ""
+    if (imc < 18.5) {
+      categoria = "Abaixo do peso."
+    }
+    else if (imc < 25) {
+      categoria = "Peso Normal."
+    }
+    else if (imc < 30) {
+      categoria = "Acima do Peso."
+    }
+    else {
+      categoria = "Obesidade."
+    }
+    const mensagem = `IMC = ${imc.toFixed(1)}\n${categoria}`
+    Alert.alert("Índice de Massa Corporal", mensagem)
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Informe o peso"
+          keyboardType='numeric'
+          onChangeText={txtPeso => this.setState({ txtPeso })}
+        />
+        <View style={styles.separator} />
+        <TextInput
+          style={styles.input}
+          placeholder="Informe a altura"
+          keyboardType='numeric'
+          onChangeText={txtAltura => this.setState({ txtAltura })}
+        />
+        <View style={styles.separator} />
+        <Button
+          title='Calcular IMC'
+          onPress={this.botaoCalcular}
+        />
+        <StatusBar />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 8,
   },
-});
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    fontSize: 22,
+    paddingHorizontal: 5
+  },
+  separator: {
+    marginBottom: 20
+  }
+})
